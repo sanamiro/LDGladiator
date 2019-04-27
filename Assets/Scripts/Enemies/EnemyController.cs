b4f55a276a3a067face8dbaac66440a83a30d3da
ftan <ftan@seven-shapes.com>
2019-04-27 14:30:05 +0200

    Update on Enemy test Scene

--- Assets/Scripts/Enemies/EnemyController.cs
+++ Assets/Scripts/Enemies/EnemyController.cs
@@ -1,18 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyController : MonoBehaviour
 {
+    public NavMeshAgent navAgent;
+    public Transform HeroPos;
+
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        MoveEnemy();
     }
+
+    public void UpdateHeroPosition(Transform newPos)
+    {
+        HeroPos = newPos;
+    }
+
+    private void MoveEnemy()
+    {
+        navAgent.SetDestination(HeroPos.position);
+    }
+
+
+    #region GETTER/SETTER
+
+
+
+    #endregion
 }

745a53e700be93c56b223c4bbb79bb81f3393979
Lucas Guedon <guedon.lucas@laposte.net>
2019-04-27 16:38:17 +0200

    Start parade

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,23 +4,35 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private enum ActionState
+    {
+        Attack,
+        AttackCooldown,
+        Parry,
+        None
+    }
 
     public float Speed;
     public Animator Animator;
     public WeaponController WeaponCollision;
+    public float AttackDuration;
+    public float AttackCooldown;
 
+    //Movement
     private Rigidbody rigidBody;
     private Vector2 velocity = Vector2.zero;
     private Vector2 targetVelocity = Vector2.zero;
-    private bool facingRight = true;
-    private bool attack = false;
 
-    private static Plane planeXZ;
+    //Attack
+    private Plane planeXZ;
+    private bool attackTriggered = false;
+    private bool shieldTriggered = false;
+    private ActionState state = ActionState.None;
+    private float attackTimer;
 
     private void Awake()
     {
         rigidBody = GetComponent<Rigidbody>();
-
     }
 
     // Start is called before the first frame update
@@ -41,45 +53,74 @@ public class PlayerController : MonoBehaviour
             Animator.SetFloat("SpeedY", targetVelocity.y);
         }
 
-        attack = Input.GetMouseButton(0);
+        if (Input.GetMouseButtonDown(0)) attackTriggered = true;
+        shieldTriggered = Input.GetMouseButtonDown(0);
 
-        Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
-
-        float dist;
-        if (planeXZ.Raycast(mouseRay, out dist))
+        if (state != ActionState.Attack)
         {
-            transform.LookAt(mouseRay.GetPoint(dist));
+            Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            float dist;
+            if (planeXZ.Raycast(mouseRay, out dist))
+            {
+                transform.LookAt(mouseRay.GetPoint(dist));
+            }
         }
     }
 
     private void FixedUpdate()
     {
-        WeaponCollision.gameObject.SetActive(attack);
+        float currentSpeed = Speed;
+
+        if (state == ActionState.Parry) currentSpeed /= 2;
+
+        Move(targetVelocity * currentSpeed * Time.fixedDeltaTime);
+
+        if (attackTimer > 0)
+        {
+            attackTimer -= Time.fixedDeltaTime;
+        }
+        switch(state)
+        {
+            case ActionState.None:
+                if (shieldTriggered)
+                {
+                    state = ActionState.Parry;
+                }
+                else if (attackTriggered)
+                {
+                    attackTriggered = false;
+                    state = ActionState.Attack;
+                    attackTimer = AttackCooldown;
+                    WeaponCollision.gameObject.SetActive(true);
+                }
+                break;
+            case ActionState.Attack:
+                if (attackTimer <= 0)
+                {
+                    state = ActionState.AttackCooldown;
+                    attackTimer = AttackDuration;
+                    WeaponCollision.gameObject.SetActive(false);
+                }
+                break;
+            case ActionState.AttackCooldown:
+                if (attackTimer <= 0)
+                {
+                    state = ActionState.None;
+                }
+                break;
+            case ActionState.Parry:
+                if (!shieldTriggered)
+                {
+                    state = ActionState.None;
+                }
+                break;
+        }
         
-        Move(targetVelocity * Speed * Time.fixedDeltaTime);
     }
 
     private void Move(Vector2 targetSpeed)
     {
         rigidBody.velocity = new Vector3(targetSpeed.x, 0, targetSpeed.y);
-
-        /*if (targetSpeed.x > 0 && !facingRight)
-        {
-            Flip();
-        }
-        else if (targetSpeed.x < 0 && facingRight)
-        {
-            Flip();
-        }*/
     }
-
-
-    /*private void Flip()
-    {
-        facingRight = !facingRight;
-
-        Vector3 theScale = transform.localScale;
-        theScale.x *= -1;
-        transform.localScale = theScale;
-    }*/
 }

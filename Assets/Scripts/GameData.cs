bb0bca907f8a47cfc174744d261e06e8af467a56
Lucas Guedon <guedon.lucas@laposte.net>
2019-04-27 23:40:55 +0200

    Add Level loading

---
+++ Assets/Scripts/GameData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class GameData : MonoBehaviour
+{
+    public List<Stage> stages;
+
+    private void Awake()
+    {
+        GameManager.stages = stages;
+        GameManager.InitFirstStage();
+    }
+
+}
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,15 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public static class GameManager
 {
+    public static List<Stage> stages;
+
     private static PlayerController player;
+    private static EnemySpawner spawner;
 
     // Player info
     private static float playerHealth;
     private static int playerMoney;
     private static EquipmentInfo playerEquipment;
+
+    // Stage Info
+    private static int currentStage = -1;
 
     // Wave info
     private static int waveCount;
@@ -21,12 +29,42 @@ public static class GameManager
     private static int killCount;
     private static int killsNeededForNextWave;
 
-    public static void InitStage(PlayerController player, int waveCount)
+    public static void InitFirstStage()
     {
-        GameManager.player = player;
-        GameManager.waveCount = waveCount;
+        if (currentStage == -1)
+        {
+            currentStage = 0;
+            InitStage();
+        }
+    }
+
+    public static void LoadNextStage()
+    {
+        currentStage++;
+
+        if (currentStage >= stages.Count)
+        {
+            //TODO Fin ?
+        }
+        else
+        {
+            //Open Arena Scene
+            
+            SceneManager.LoadScene("BattleArena");
+            InitStage();
+
+            //Reset Player Attributes
+            player.Health = playerHealth;
+            //player.Equipment = playerEquipment;
+        }
+    }
+
+    private static void InitStage()
+    {
+        player = UnityEngine.Object.FindObjectOfType<PlayerController>();
+        spawner = UnityEngine.Object.FindObjectOfType<EnemySpawner>();
+        waveCount = stages[currentStage].Waves.Count;
         bonusMoney = 0;
-        killCount = 0;
         currentWave = 0;
         LoadWave();
     }
@@ -43,15 +81,28 @@ public static class GameManager
 
     private static void LoadWave()
     {
+        killCount = 0;
         if (currentWave >= waveCount)
         {
             OnWinStage();
         }
         else
         {
-            //TODO Load new wave data
+            //Load new wave data
 
-            //Start wave
+            List<EnemyData> enemiesToSpawn = stages[currentStage].Waves[currentWave].Enemies;
+
+            killsNeededForNextWave = 0;
+            foreach (EnemyData enemy in enemiesToSpawn)
+            {
+                killsNeededForNextWave += enemy.Count;
+
+                //Spawn enemy
+                for (int i = 0; i < enemy.Count; i++)
+                {
+                    spawner.SpawnEnemy(spawner.transform, enemy.EnemyType);
+                }
+            }
         }
     }
 
@@ -60,7 +111,8 @@ public static class GameManager
         playerHealth = player.Health;
         //playerEquipment = player.Equipment;
 
-        //TODO Load Marchant scene
+        //Load Marchant scene
+        SceneManager.LoadScene("Store");
     }
 
 }

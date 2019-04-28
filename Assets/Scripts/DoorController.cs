e5673ab0d3699cd30e8d3c482b8d18894d2c9332
ftan <ftan@seven-shapes.com>
2019-04-28 18:08:15 +0200

    Added doors to game

---
+++ Assets/Scripts/DoorController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorController : MonoBehaviour
+{
+
+    private bool hasClosed = false;
+
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!hasClosed)
+            CloseDoor();
+    }
+
+    public void CloseDoor()
+    {
+        if (transform.position.y <= 2.40f)
+            transform.position = new Vector3(transform.position.x, transform.position.y + 0.1f, transform.position.z);
+        else
+            hasClosed = true;
+
+    }
+}

07e66bc95fd75c06746b01c23b2db4cdabf5cd7a
ftan <ftan@seven-shapes.com>
2019-04-28 12:23:43 +0200

    V2 of camera, can still work better

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -15,6 +15,61 @@ public class CameraController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(PlayerPos.position.x, 20, PlayerPos.position.z + 30);
+        Vector3 newPos = new Vector3(0,20,0);
+        float newX = PlayerPos.position.x;
+        float newZ = PlayerPos.position.z;
+
+        /*if (Mathf.Pow((newX / 19), 2) + Mathf.Pow((newZ / 15), 2) > 1)         //Si on sort de l'ellipse voulue
+        {
+            if (newX <= 0 && newZ != 0)                                         //Si on est à gauche du centre (y positifs)
+            {
+                float x = newX;
+                float y = (newX / newZ) * x;
+                while (Mathf.Pow((x / 19), 2) + Mathf.Pow((y / 15), 2) > 1)
+                {
+                    x += 0.1f;
+                    y = (newX / newZ) * x;
+                }
+
+                newPos.x = x;
+                newPos.z = (newX / newZ) * x + 30;
+            }
+
+            else if (newX >= 0 && newZ != 0)                                    //Si on est à droite du centre (x positifs)
+            {
+                float x = newX;
+                float y = (newX / newZ) * x;
+                while (Mathf.Pow((x / 19), 2) + Mathf.Pow((y / 15), 2) > 1)
+                {
+                    x -= 0.1f;
+                    y = (newX / newZ) * x;
+                }
+
+                newPos.x = x;
+                newPos.z = (newX / newZ) * x + 30;
+            }
+
+            else if (newX <= 0 && newZ == 0)                                    //Si on est sur l'axe des abcisses (x+ ou x-)
+                newPos = new Vector3(22, 0, 30);
+            else if (newX >= 0 && newZ == 0)
+                newPos = new Vector3(-22, 0, 30);
+        }
+        else
+            newPos = new Vector3(PlayerPos.position.x, 20, PlayerPos.position.z + 30);*/
+
+        if (newX <= -15)
+            newX = -15;
+        if (newX >= 15)
+            newX = 15;
+
+        if (newZ <= -15)
+            newZ = -15;
+        if (newZ >= 10)
+            newZ = 10;
+
+        newPos.x = newX;
+        newPos.z = newZ + 30;
+
+        transform.position = newPos;
     }
 }

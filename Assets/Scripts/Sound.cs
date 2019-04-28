8998aa9a9b9099c94c0406315f7d07a96f0ebb12
kpedatte <kpedatte@seven-shapes.com>
2019-04-28 18:16:39 +0200

    Add Sounds and mixer

---
+++ Assets/Scripts/Sound.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEditor;
+
+[System.Serializable]
+public class Sound
+{
+    public string name;
+
+    public AudioMixerGroup mixerGroup;
+
+    public bool loop;
+
+    [Range(0f, 1f)] public float volume;
+
+    public bool followingSounds;
+    public bool randomSounds;
+
+    public int currentIndex;
+    public int rndIndex;
+
+    public int clipsNumber;
+    [HideInInspector] public AudioClip clip;
+    [HideInInspector] public AudioClip[] multipleClips;
+
+    [HideInInspector] public AudioSource source;
+}

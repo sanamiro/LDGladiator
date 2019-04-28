f69d2415dee4fc3ec51fd06044a5dbff717f0abe
ftan <ftan@seven-shapes.com>
2019-04-28 14:08:16 +0200

    Added darkener prefab

---
+++ Assets/Scripts/DarkenerController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DarkenerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public Image darkenerImage;
+    public bool isGoingDark = false;
+    public bool isGoingLight = false;
+
+    void Start()
+    {
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isGoingDark)
+            InDarkener();
+        else if (isGoingLight)
+            OutDarkener();
+    }
+
+    public void InDarkener()
+    {
+        float currentGamma = darkenerImage.color.a;
+        if (currentGamma >= 0.99f)
+        {
+            Debug.Log("InDarkenerOver");
+            isGoingDark = false;
+        }
+        else
+        {
+            Color newColor = new Color(0, 0, 0, currentGamma + 0.025f);
+            darkenerImage.color = newColor;
+        }
+    }
+
+    public void OutDarkener()
+    {
+        float currentGamma = darkenerImage.color.a;
+
+        if (currentGamma <= 0.01f)
+        {
+            Debug.Log("OutDarkenerOver");
+            isGoingLight = false;
+        }
+        else
+        {
+            Color newColor = new Color(0, 0, 0, currentGamma - 0.05f);
+            darkenerImage.color = newColor;
+        }
+    }
+
+}
